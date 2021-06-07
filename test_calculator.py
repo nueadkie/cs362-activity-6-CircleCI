e2b4e7fa8095055043ef6dccbc949fdf054aae34
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-06-06 22:03:18 -0700

    Set-up unit tests and virtual environment

---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+"""
+Unit tests using pytest for calculator
+"""
+
+
+import calculator
+
+
+class TestCalculator:
+
+    def test_add(self):
+        assert 5 == calculator.add(1, 5)
+  
+    def test_subtract(self):
+        assert 2 == calculator.subtract(4, 2)
