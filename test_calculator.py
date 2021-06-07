a56be5f80b29f9f746c86d7d02f3c1cda0757bf1
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-06-06 22:23:14 -0700

    Fixed add test

--- test_calculator.py
+++ test_calculator.py
@@ -9,7 +9,7 @@ import calculator
 class TestCalculator:
 
     def test_add(self):
-        assert 5 == calculator.add(1, 5)
+        assert 6 == calculator.add(1, 5)
 
     def test_subtract(self):
         assert 2 == calculator.subtract(4, 2)
