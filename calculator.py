e80dec3c93d13211fc91df1353947e64f49ef61d
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-06-06 22:06:31 -0700

    Fixed linter errors

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Basic calculator app.
 """
 
+
 def add(a, b):
-  return a + b
+    return a + b
+
 
 def subtract(a, b):
-  return a - b
+    return a - b
--- test_calculator.py
+++ test_calculator.py
@@ -10,6 +10,6 @@ class TestCalculator:
 
     def test_add(self):
         assert 5 == calculator.add(1, 5)
-  
+
     def test_subtract(self):
         assert 2 == calculator.subtract(4, 2)
